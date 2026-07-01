1a786f1daf69762f2e62b98419a6fa182b000dcc
Raka200juta <rakasatryo03@gmail.com>
2026-07-01 20:35:14 +0700

    refine app

--- app.py
+++ app.py
@@ -17,7 +17,9 @@ from firebase_admin import credentials, firestore
 @st.cache_resource
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate("serviceAccountKey.json")
+        # Mengonversi format Secrets menjadi dictionary Python
+        firebase_creds = dict(st.secrets["firebase_secrets"])
+        cred = credentials.Certificate(firebase_creds)
         firebase_admin.initialize_app(cred)
     return firestore.client()
 
